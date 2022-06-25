ff0c5f3968e0b0758541f12cb19b49ffa30c3eaf
Billy <lrdweaver@gmail.com>
2022-06-25 18:04:27 -0400

    New idea for RPS

--- app.js
+++ app.js
@@ -1,5 +1,19 @@
 /* Rock, Paper, Scissors
 
+/*    
+
+HTML
+<h1>Rock Paper Scissors!</h1>
+        <select class="selection">
+            <option>Enter-Selection</option>
+            <option id="1">Rock!</option>
+            <option id="2">Paper!</option>
+            <option id="3">Scissors!</option>
+        </select>
+        <button class="rps-btn">Rock, Paper, Scissors</button>
+        <p></p>
+HTML
+
 1. begin with a function called computerPlay that will 
 randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
 
@@ -9,96 +23,67 @@ random math algorithm and returns either 1, 2 or 3.
 
 */
 
+// let rpsBtn = document.querySelector(".rps-btn");
+// let playerSelection = document.querySelector(".selection");
+// let paragraph = document.querySelector("p");
+
+// rpsBtn.addEventListener("click", playRound);
+
+// function updateValue() {
+//     if (playerSelection.value == "Rock!") {
+//         return 1
+//     } else if (playerSelection.value == "Paper!") {
+//         return 2
+//     } else if (playerSelection.value == "Scissors!") {
+//         return 3
+//     } else {
+//         return null
+//     }
+// }
+
+// function computerPlay(){
+//     var number = Math.floor(Math.random() * 3) + 1;
+//     if (number == 1) {
+//         return number;
+//     } else if (number == 2){
+//         return number;
+//     } else if (number == 3) {
+//         return number;
+//     } else {
+//         return null
+//     }
+// }
 
 
+// function playRound() {
+//     console.log(updateValue(), computerSelection)
+//         if ((updateValue() == 1) && (computerSelection == 1)) {
+//             paragraph.innerText = "Draw!";
+//         } else if ((updateValue() == 1) && (computerSelection == 2)) {
+//             paragraph.innerText = "You lose! Paper beats Rock!";
+//         } else if ((updateValue() == 1) && (computerSelection == 3)) {
+//             paragraph.innerText = "you Win! Rock beats Scissors!";
+//         } else if ((updateValue() == 2) && (computerSelection == 2)) {
+//             paragraph.innerText = "Draw!";
+//         } else if ((updateValue() == 2) && (computerSelection == 1)) {
+//             paragraph.innerText = "You Win! Paper beats Rock!";
+//         } else if ((updateValue() == 2) && (computerSelection == 3)) {
+//             paragraph.innerText = "You Lose! Scissors beat Paper!";
+//         } else if ((updateValue() == 3) && (computerSelection == 3)) {
+//             paragraph.innerText = "Draw!";
+//         } else if ((updateValue() == 3) && (computerSelection == 1)) {
+//             paragraph.innerText = "You Lose! Rock beats Scissors!";
+//         } else if ((updateValue() == 3) && (computerSelection == 2)) {
+//             paragraph.innerText = "You Win! Scissors beat Paper!";
+//         } else {
+//             paragraph.innerText = "Please make your selection";
+//         }
+//     }
 
+// const computerSelection = computerPlay()
+// const newValue = updateValue();
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-let rpsBtn = document.querySelector(".rps-btn");
-let playerSelection = document.querySelector(".selection");
-let paragraph = document.querySelector("p");
-
-let newValue = updateValue();
-
-rpsBtn.addEventListener("click", playRound);
-
-function updateValue() {
-    if (playerSelection.value == "Rock!") {
-        return 1
-    } else if (playerSelection.value == "Paper!") {
-        return 2
-    } else if (playerSelection.value == "Scissors!") {
-        return 3
-    } else {
-        return null
-    }
-}
-
-function computerPlay(){
-    const number = Math.floor(Math.random() * 3) + 1;
-    if (number == 1) {
-        console.log("Rock!")
-            return 'Rock!'
-    }   else if (number == 2){
-        console.log("Paper!")
-            return 'Paper!'
-    }   else if (number == 3) {
-        console.log("Scissors!")
-            return 'Scissors!'
-    }
-}
-
-function playRound() {
-    let number = Math.floor(Math.random() * 3) + 1;
-    console.log(updateValue(), number)
-        if ((updateValue() == 1) && (number == 1)) {
-            return paragraph.innerText = "Draw!"
-        } else if ((updateValue() == 1) && (number == 2)) {
-            return paragraph.innerText = "You lose! Paper beats Rock!"
-        } else if ((updateValue() == 1) && (number == 3)) {
-            return paragraph.innerText = "you Win! Rock beats Scissors!"
-        } else if ((updateValue() == 2) && (number == 2)) {
-            return paragraph.innerText = "Draw!"
-        } else if ((updateValue() == 2) && (number == 1)) {
-            return paragraph.innerText = "You Win! Paper beats Rock!"
-        } else if ((updateValue() == 2) && (number == 3)) {
-            return paragraph.innerText = "You Lose! Scissors beat Paper!"
-        } else if ((updateValue() == 3) && (number == 3)) {
-            return paragraph.innerText = "Draw!"
-        } else if ((updateValue() == 3) && (number == 1)) {
-            return paragraph.innerText = "You Lose! Rock beats Scissors!"
-        } else if ((updateValue() == 3) && (number == 2)) {
-            return paragraph.innerText = "You Win! Scissors beat Paper!"
-        } else {
-            return paragraph.innerText = "Please make your selection"
-        }
-    }
+// rpsBtn.addEventListener("click", playRound);
 
 /*
 2. Write a function that plays a single round of Rock Paper Scissors
@@ -121,8 +106,37 @@ f.  -if playerSelection == 1 and computerValue == 1 console.log("Draw!")
     -if playerSelection == 3 and computerValue == 1 console.log("You Lose! Rock beats Scissors!")
     -if playerSelection == 3 and computerValue == 2 console.log("You Win! Scissors beats Paper!")
     -if playerSelection == 3 and computerValue == 3 console.log("Draw!")
-*/ 
+*/
 
+
+// ----- ---RPS--- -----
+// ----- Attempt 2 ----- 
+// Instead of a selector within the HTML, use 3 individual buttons
+// that call a single playRound function.
+
+// Step 1. Create 3 HTML button elements and imbed them within a variable.
+// Step 2. Write a computerPlay function that will randomly return either 'Rock', 
+// 'Paper', or 'Scissors'. This can maybe be done using 
+// arr[Math.floor(Math.random() * arr.;ength)]
+// Step 3. If user selects 'Rock' button, run the computerPlay function within a new 
+// function that compares the users selection to the computerPlay output.
+var up = document.getElementById('GFG_UP');
+var down = document.getElementById('GFG_DOWN');
+ 
+var arr = ["GFG_1", "GeeksForGeeks",
+        "Geeks", "Computer Science Portal"];
+ 
+up.innerHTML = "Click on the button to select"
+                + " random element from the"
+                + " array.<br><br>" + arr;
+ 
+function random(mn, mx) {
+    return Math.random() * (mx - mn) + mn;
+}
+ 
+function GFG_Fun() {
+    down.innerHTML = arr[Math.floor(random(1, 5))-1];
+}
 
 
 
